0592e3f80bacb06179ac703e4f756f5e6f4e7d76
erilu762@berkeley.edu <erilu762@berkeley.edu>
2022-01-29 12:56:19 -0800

    getting h ref

--- command.js
+++ command.js
@@ -17,7 +17,32 @@ module.exports = async function (msg){
         const page_url = 'https://hololive.wiki/wiki/';
         const {data} = await axios.get(page_url);
         const $ = cheerio.load(data);
-        const vtuber_href = $('')
+        //('table.wikitable')
+        // const vtuber = $("#mw-content-text > div.mw-parser-output > div > div:nth-child(1) > div > div:nth-child(TABLE#) > table:nth-child(ROW) > tbody > tr:nth-child(2) > td > p > span:nth-child(COL) > a")).attr('title');
 
+        const vtuber = $("#mw-content-text > div.mw-parser-output > div > div:nth-child(1) > div > div:nth-child(3) > table:nth-child(1) > tbody > tr:nth-child(2) > td > p > span:nth-child(1) > a").attr('title');
+
+        const table = $('table.wikitable');
+        table.find('tbody tr:nth-child(2) td').each((i, element) => {
+            const $element = $(element);
+
+            $element.find('p span').each((j, element2) => {
+                const $element2 = $(element2);
+                const vtubers = {};
+                vtubers.name = $element2.find('a').attr('href');
+                console.log(vtubers);
+            });
+
+            console.log('\n')
+
+            // const vtubers = {};
+            // vtubers.name = $element.find('p a').attr('href');
+            // console.log(vtubers);
+            // console.log($element.text());
+        });
+
+        if(typeof(vtuber) == 'string'){
+            msg.channel.send(vtuber_title);
+        }
     }
 }
